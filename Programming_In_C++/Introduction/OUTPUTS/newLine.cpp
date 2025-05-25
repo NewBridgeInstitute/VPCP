be5a311fa6b553ce67b35618df5b1af6d478a4e3
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-05-25 19:05:37 +0200

    renamed:    Programming_In_C++/.vscode/tasks.json -> Programming_In_C++/Introduction/.vscode/tasks.json 	new file:   Programming_In_C++/Introduction/OUTPUTS/newLine.cpp 	new file:   Programming_In_C++/Introduction/OUTPUTS/newLine.exe 	new file:   Programming_In_C++/Introduction/OUTPUTS/numbers.cpp 	new file:   Programming_In_C++/Introduction/OUTPUTS/numbers.exe 	renamed:    Programming_In_C++/main.cpp -> Programming_In_C++/Introduction/main.cpp 	renamed:    Programming_In_C++/main.exe -> Programming_In_C++/Introduction/main.exe

---
+++ Programming_In_C++/Introduction/OUTPUTS/newLine.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+//\n character:
+  cout << "To insert a new line in your output, you can use the backslash symbol and n to break to the next line." << "\n\n";
+//endl manipulator:
+  cout << "Another way to insert a new line, is with the endl manipulator." << endl;
+  return 0;
+}
