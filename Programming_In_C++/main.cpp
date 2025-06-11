06849c9f7cee7f483e9c899c6aeace1dc7526a17
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-06-11 16:07:15 +0200

    replied

--- Programming_In_C++/main.cpp
+++ Programming_In_C++/main.cpp
@@ -5,6 +5,6 @@ using namespace std;
 int main()
 {
     cout << "Hello world!" << endl;
-    cout << "Hello again" << endl;
+    cout << "Hello again" << endl; // Mr.Schabs replies to hello world!
     return 0;
 }
