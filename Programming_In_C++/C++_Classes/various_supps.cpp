3991ae5a4ab74c5d444834f07fbe7feb082b3f0c
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-06-11 14:19:36 +0200

    VARIOUS SUPPS (updated)

--- Programming_In_C++/C++_Classes/various_supps.cpp
+++ Programming_In_C++/C++_Classes/various_supps.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 using namespace std;
 class WithConstructor {
@@ -13,5 +14,36 @@ int main(void) {
 	o2 = new WithConstructor;
 	o1.Display();
 	o2 -> Display();
+	return 0;
+}
+*/
+
+// HERE WE CALL THEM ALL OUT IN THIS EXAMPLE
+#include <iostream>
+using namespace std;
+class A {
+public:
+	A(A &src) { cout << "copying A..." << endl; }
+	A(void) { }
+	void Do(void) { cout << "A is doing something" << endl; }
+};
+class B {
+public:
+	B(B &src) { cout << "copying B..." << endl; }
+	B(void){ }
+	void Do(void) { cout << "B is doing something" << endl; }
+};
+class Compo {
+public:
+    Compo(Compo &src) : f1(src.f1), f2(src.f2) { cout << "Copying Compo..." << endl; } //here we call them all out
+	Compo(void) { } ;
+	A f1;
+	B f2;
+};
+int main(void) {
+	Compo  co1;
+	Compo  co2 = co1;
+	co2.f1.Do();
+	co2.f2.Do();
 	return 0;
 }
