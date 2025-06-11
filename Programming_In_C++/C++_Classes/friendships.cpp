ab89236082c962bb80c88c289cfe1950e379e1e9
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-06-11 21:08:39 +0200

    Update friendships.cpp
    Brief description of each line of code.

--- Programming_In_C++/C++_Classes/friendships.cpp
+++ Programming_In_C++/C++_Classes/friendships.cpp
@@ -1,19 +1,25 @@
-/*class Class {
-friend class Friend;
-private:
-    int field;
-    void print(void)
-    { cout << "It's a secret, that field = " << field << endl; }
-};
-class Friend {
-public:
-    void DoIt (Class &c) { c.field = 100; c.print(); }
-};
-int main(void) {
-    Class o;
-    Friend f;
+class Class { // Defining class named 'Class'
+friend class Friend; // Grants 'Friend' access to private members of 'Class'
 
-    f.DoIt(o);
-    return 0;
+private:
+    int field; // A private integer member variable, not accessible outside this class
+
+    void print(void) // A private function that prints the value of 'field'
+    { cout << "It's a secret, that field = " << field << endl; } //Outputs the value of "field"
+};
+
+class Friend { // Defines another class named 'Friend'
+public:
+    void DoIt (Class &c) { // A public function that modifies 'Class' object
+         c.field = 100; // Changes the private variable 'field' inside 'Class' (allows due to 'friend' declaration)
+         c.print(); //Calls the private 'print' function of 'Class' to display the updated value
+    }
+};
+int main(void) { // Is the entry point of the program
+    Class o; // Creates an instance of "CLass"
+    Friend f; // Creates an instance of 'Friend'
+
+    f.DoIt(o); // Calls 'DoIt' function on 'o', modifying 'field' and printing it's new value
+    return 0; // And then 'return 0' of cause indicates successful execution(if you did not know by now:))
 }
-*/
+
