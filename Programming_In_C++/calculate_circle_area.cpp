5fd80f2094085f43662179751c5aefad2e1bfd32
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-06-12 11:52:56 +0200

    friends of classes exercise:

--- Programming_In_C++/calculate_circle_area.cpp
+++ Programming_In_C++/calculate_circle_area.cpp
@@ -17,3 +17,53 @@ int main () {
     return 0;
 }
 */
+/*FRIENDS OF CLASSES EXERCISE:*/
+
+#include <iostream>
+using namespace std;
+class Sport {
+private:
+    int players;
+    void print(void) { cout << "This sport has: " << players << " players" << endl;}
+public:
+    friend class Golf;
+    friend class Baseball;
+};
+class Golf {
+public:
+    void play (Sport &s) { s.players = 853; s.print();}
+};
+class Baseball {
+public:
+    void play(Sport &s) { s.players = 962; s.print();}
+};
+int main() {
+    Sport sport;
+    Golf golf;
+    Baseball baseball;
+    golf.play(sport);
+    baseball.play(sport);
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
