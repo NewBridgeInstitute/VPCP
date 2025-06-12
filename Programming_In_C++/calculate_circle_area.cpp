4eb4bfbd842c1cd750cdbd7be735286eb7baa4d6
Tyler Van Wyk <tylervanwyk01@outlook.com>
2025-06-12 10:18:22 +0200

    const e.g

---
+++ Programming_In_C++/calculate_circle_area.cpp
@@ -0,0 +1,19 @@
+/*
+// Calculate Circle Area
+#include <iostream>
+using namespace std;
+
+const double PI = 3.14159;
+
+int main () {
+    double radius, area;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+
+    area = PI * radius * radius;
+
+    cout <<"Ther area of the circle is: " << area <<endl;
+
+    return 0;
+}
+*/
